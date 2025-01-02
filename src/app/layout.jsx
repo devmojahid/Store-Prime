ab3dd6cb86ad305e3ff2f35c18f4c3c3afaa68d5
Mojahid <raofahmedmojahid@gmail.com>
2025-01-02 12:11:28 +0600

    initial

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
+});
+
+export const metadata = {
+  title: {
+    default: "Store Prime - Complete E-Commerce Solution",
+    template: "%s | Store Prime"
+  },
+  description: "Next-generation E-Commerce Solution for your business",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>{children}</body>
+    </html>
+  );
+}
